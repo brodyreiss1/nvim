33c0ce9c8289299974825f99322886e6c365127f
Brody Reiss <cbreiss311@gmail.com>
2025-04-04 09:33:51 -0500

    updated python lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,22 +5,9 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 
 
 -- Python LSP setup
-lspconfig.pyright.setup({
-	on_new_config = function(new_config, root_dir)
-		local pipfile_exists = require("lspconfig").util.search_ancestors(root_dir, function(path)
-			local pipfile = require("lspconfig").util.path.join(path, "Pipfile")
-			if require("lspconfig").util.path.is_file(pipfile) then
-				return true
-			else
-				return false
-			end
-		end)
-
-		if pipfile_exists then
-			new_config.cmd = { "pipenv", "run", "pyright-langserver", "--stdio" }
-		end
-	end,
-})
+lspconfig.pyright.setup{
+    capabilities = capabilities,
+}
 
 -- Typescript LSP setup
 lspconfig.ts_ls.setup{
